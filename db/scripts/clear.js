44b93b5a826008f3b0d1d6e9088034cc8e843267
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-06-03 10:45:40 +0200

    fix tag function args

--- db/scripts/clear.js
+++ db/scripts/clear.js
@@ -2,17 +2,20 @@ const PrismaClient = require("@prisma/client");
 
 const prisma = new PrismaClient.PrismaClient();
 
+/**
+ * @description naive clearing of tables, this needs update every time we add new tables,
+ * reasong being the use of tag functions, where we cannot use interpolation of values
+ * as it will break the args of the tag function.
+ * 
+ */
 async function clearAllTables() {
-    const tables = [
-        PrismaClient.Prisma.ModelName.User,
-        PrismaClient.Prisma.ModelName.Post,
-    ];
     try {
-        await prisma.$executeRaw("PRAGMA foreign_keys = OFF;");
+        await prisma.$executeRaw`PRAGMA foreign_keys = OFF;`;
 
-        await Promise.all(tables.map(x => prisma.$executeRaw(`DELETE FROM ${x};`)));
+        await prisma.$executeRaw`DELETE FROM User;`;
+        await prisma.$executeRaw`DELETE FROM Post;`;
 
-        await prisma.$executeRaw("PRAGMA foreign_keys = ON;");
+        await prisma.$executeRaw`PRAGMA foreign_keys = ON;`;
 
         console.log("All tables cleared successfully.");
     } catch (error) {
